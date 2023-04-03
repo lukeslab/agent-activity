abeaede595b68ac0a8a0c0e185761b93f7ca14ab
Luke Padawer <54520310+lukeslab@users.noreply.github.com>
2023-04-03 15:24:49 -0400

    update agent history model to improve navigation of the history object.

--- server/server.js
+++ server/server.js
@@ -13,20 +13,41 @@ const agentSchema = new Schema({
   chatId: String,
   firstName: String,
   lastName: String,
-  history: [{}],
+  history: {},
   avatar: String,
 })  
 
 exports = {
   onAgentActivityCreateHandler: async function (payload) {
-    
-    const { data: { agent_activity: {availability_event_type: eventType} } } = payload
-    console.log(eventType)
-    if(eventType !== "Intelliassign") {
-      console.log("Event type is not intelliassign, exiting.")
+    // deconstruct payload
+    const { 
+      data: { 
+        agent_activity: { 
+          availability_event_type,
+          status 
+        },
+        actor: {
+          first_name,
+          last_name,
+          id,
+          avatar: { url }
+        }, 
+      }, 
+      timestamp
+    } = payload
+    // get the year, month, and day from payload timestamp
+    const date = new Date(timestamp*1000)
+    const year = date.getFullYear()
+    const month = date.getMonth()
+    const day = date.getDate() // this returns the day of the month
+
+    // exit if not intelliassign
+    if(availability_event_type !== "Intelliassign") {
+      console.log(`Event type is ${availability_event_type}. Event type is not intelliassign, exiting.`)
       return
     }
 
+    // check db connection
     if (mongoose.connection.readyState !== 1){
       console.log('ReadyState changed. Reconnecting to db.')
       await connectDB(payload.iparams.mongodb_password)
@@ -37,42 +58,61 @@ exports = {
       mongoose.connection.on('error', console.error.bind(console, "connection error: "))
     }
     
+    // establish db model
     const Agent = mongoose.models.Agent || mongoose.model('Agent', agentSchema)
 
+
     try {
-      const agent = await Agent.findOne({ chatId: payload.data.actor.id })
-  
+      const agent = await Agent.findOne({ chatId: id })
+      
+      // if agent already has a record, update it.
       if(agent) {
+        const history = agent.history
+        
+        //Check that the history has the year, month, day, and activity property for the given date.
+        if(!history.hasOwnProperty(year)) history[year] ={}
+        if(!history[year].hasOwnProperty(month)) history[year][month] = {}
+        if(!history[year][month].hasOwnProperty(day)) history[year][month][day] = {}
+        if(!history[year][month][day].hasOwnProperty('activity')) history[year][month][day]['activity'] = []
+
+        // Update the activity property for the given date
+        history[year][month][day]['activity'] = [
+          ...history[year][month][day]['activity'],
+          {
+            status,
+            timestamp
+          }
+        ]
+        console.log(year, month, day, history)
         console.log(`Agent ${agent.firstName} ${agent.lastName} (${agent.chatId}) found, updating history.`)
         return Agent.updateOne(
           {
-            chatId: payload.data.actor.id
+            chatId: id
           },
           {
-            history: [
-              ...agent.history,
-              {
-                status: payload.data.agent_activity.status,
-                timestamp: payload.timestamp
-              }
-            ]
+            history
           }
         )
       } else {
-        const id = payload.data.actor.id;
-        const firstName = payload.data.actor.first_name;
-        const lastName = payload.data.actor.last_name;
-        console.log(`Agent not found, creating document for Agent ${firstName} ${lastName} (${id}).`)
-  
+        // If no agent is found, use payload data to create a document for the agent.
+        console.log(`Agent not found, creating document for Agent ${first_name} ${last_name} (${id}).`)
+
+        // Initialize history object
+        const history = {}
+        history[year] = {}
+        history[year][month] = {}
+        history[year][month][day] = {}
+        history[year][month][day]['activity'] = [{
+          status,
+          timestamp
+        }] 
+
         return await Agent.create({
           chatId: id,
-          firstName: firstName,
-          lastName: lastName,
-          history: [{
-            status: payload.data.agent_activity.status,
-            timestamp: payload.timestamp
-          }],
-          avatar: payload.data.actor.avatar.url
+          firstName: first_name,
+          lastName: last_name,
+          history,
+          avatar: url
         })
           .then( data => console.log('Agent activity recorded successfully', data) )
       }
